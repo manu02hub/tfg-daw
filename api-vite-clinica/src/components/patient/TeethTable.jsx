075859023b4da3da421ab5446fffb993bd87e1b6
manu02hub <malonso@alumnos.imf.com>
2023-05-07 23:15:55 +0200

    Mostrar dientes para tratamiento

--- api-vite-clinica/src/components/patient/TeethTable.jsx
+++ api-vite-clinica/src/components/patient/TeethTable.jsx
@@ -12,62 +12,62 @@ function TeethTable() {
   const arrayTeethUp = [
     {
       tooth: [
-        { id: 1, pieza: "18c", img: url + "18C" },
-        { id: 2, pieza: "17c", img: url + "17C" },
-        { id: 3, pieza: "16c", img: url + "16C" },
-        { id: 4, pieza: "15c", img: url + "15C" },
-        { id: 5, pieza: "14c", img: url + "14C" },
-        { id: 6, pieza: "13c", img: url + "13C" },
-        { id: 7, pieza: "12c", img: url + "12C" },
-        { id: 8, pieza: "11c", img: url + "11C" },
-        { id: 9, pieza: "21c", img: url + "21C" },
-        { id: 10, pieza: "22c", img: url + "22C" },
-        { id: 11, pieza: "23c", img: url + "23C" },
-        { id: 12, pieza: "24c", img: url + "24C" },
-        { id: 13, pieza: "25c", img: url + "25C" },
-        { id: 14, pieza: "26c", img: url + "26C" },
-        { id: 15, pieza: "27c", img: url + "27C" },
-        { id: 16, pieza: "28c", img: url + "28C" },
+        { id: 1, pieza: "18c", img: url + "18C.png" },
+        { id: 2, pieza: "17c", img: url + "17C.png" },
+        { id: 3, pieza: "16c", img: url + "16C.png" },
+        { id: 4, pieza: "15c", img: url + "15C.png" },
+        { id: 5, pieza: "14c", img: url + "14C.png" },
+        { id: 6, pieza: "13c", img: url + "13C.png" },
+        { id: 7, pieza: "12c", img: url + "12C.png" },
+        { id: 8, pieza: "11c", img: url + "11C.png" },
+        { id: 9, pieza: "21c", img: url + "21C.png" },
+        { id: 10, pieza: "22c", img: url + "22C.png" },
+        { id: 11, pieza: "23c", img: url + "23C.png" },
+        { id: 12, pieza: "24c", img: url + "24C.png" },
+        { id: 13, pieza: "25c", img: url + "25C.png" },
+        { id: 14, pieza: "26c", img: url + "26C.png" },
+        { id: 15, pieza: "27c", img: url + "27C.png" },
+        { id: 16, pieza: "28c", img: url + "28C.png" },
       ],
     },
     {
       tooth: [
-        { id: 17, pieza: "18b", img: url + "18B" },
-        { id: 18, pieza: "17b", img: url + "17B" },
-        { id: 19, pieza: "16b", img: url + "16B" },
-        { id: 20, pieza: "15b", img: url + "15B" },
-        { id: 21, pieza: "14b", img: url + "14B" },
+        { id: 17, pieza: "18b", img: url + "18B.png" },
+        { id: 18, pieza: "17b", img: url + "17B.png" },
+        { id: 19, pieza: "16b", img: url + "16B.png" },
+        { id: 20, pieza: "15b", img: url + "15B.png" },
+        { id: 21, pieza: "14b", img: url + "14B.png" },
         { id: 22, pieza: "", img: "" },
         { id: 23, pieza: "", img: "" },
         { id: 24, pieza: "", img: "" },
         { id: 25, pieza: "", img: "" },
         { id: 26, pieza: "", img: "" },
         { id: 27, pieza: "", img: "" },
-        { id: 28, pieza: "24b", img: url + "24B" },
-        { id: 29, pieza: "25b", img: url + "25B" },
-        { id: 30, pieza: "26b", img: url + "26B" },
-        { id: 31, pieza: "27b", img: url + "27B" },
-        { id: 32, pieza: "28b", img: url + "28B" },
+        { id: 28, pieza: "24b", img: url + "24B.png" },
+        { id: 29, pieza: "25b", img: url + "25B.png" },
+        { id: 30, pieza: "26b", img: url + "26B.png" },
+        { id: 31, pieza: "27b", img: url + "27B.png" },
+        { id: 32, pieza: "28b", img: url + "28B.png" },
       ],
     },
     {
       tooth: [
-        { id: 33, pieza: "18a", img: url + "18A" },
-        { id: 34, pieza: "17a", img: url + "17A" },
-        { id: 35, pieza: "16a", img: url + "16A" },
-        { id: 36, pieza: "15a", img: url + "15A" },
-        { id: 37, pieza: "14a", img: url + "14A" },
-        { id: 38, pieza: "13a", img: url + "13A" },
-        { id: 39, pieza: "12a", img: url + "12A" },
-        { id: 40, pieza: "11a", img: url + "11A" },
-        { id: 41, pieza: "21a", img: url + "21A" },
-        { id: 42, pieza: "22a", img: url + "22A" },
-        { id: 43, pieza: "23a", img: url + "23A" },
-        { id: 44, pieza: "24a", img: url + "24A" },
-        { id: 45, pieza: "25a", img: url + "25A" },
-        { id: 46, pieza: "26a", img: url + "26A" },
-        { id: 47, pieza: "27a", img: url + "27A" },
-        { id: 48, pieza: "28a", img: url + "28A" },
+        { id: 33, pieza: "18a", img: url + "18A.png" },
+        { id: 34, pieza: "17a", img: url + "17A.png" },
+        { id: 35, pieza: "16a", img: url + "16A.png" },
+        { id: 36, pieza: "15a", img: url + "15A.png" },
+        { id: 37, pieza: "14a", img: url + "14A.png" },
+        { id: 38, pieza: "13a", img: url + "13A.png" },
+        { id: 39, pieza: "12a", img: url + "12A.png" },
+        { id: 40, pieza: "11a", img: url + "11A.png" },
+        { id: 41, pieza: "21a", img: url + "21A.png" },
+        { id: 42, pieza: "22a", img: url + "22A.png" },
+        { id: 43, pieza: "23a", img: url + "23A.png" },
+        { id: 44, pieza: "24a", img: url + "24A.png" },
+        { id: 45, pieza: "25a", img: url + "25A.png" },
+        { id: 46, pieza: "26a", img: url + "26A.png" },
+        { id: 47, pieza: "27a", img: url + "27A.png" },
+        { id: 48, pieza: "28a", img: url + "28A.png" },
       ],
     },
   ];
@@ -82,62 +82,62 @@ function TeethTable() {
   const arrayTeethDown = [
     {
       tooth: [
-        { id: 49, pieza: "48a", img: url+"48A" },
-        { id: 50, pieza: "47a", img: url+"48A" },
-        { id: 51, pieza: "46a", img: url+"48A" },
-        { id: 52, pieza: "45a", img: url+"48A" },
-        { id: 53, pieza: "44a", img: url+"48A" },
-        { id: 54, pieza: "43a", img: url+"48A" },
-        { id: 55, pieza: "42a", img: url+"48A" },
-        { id: 56, pieza: "41a", img: url+"48A" },
-        { id: 57, pieza: "31a", img: url+"48A" },
-        { id: 58, pieza: "32a", img: url+"48A" },
-        { id: 59, pieza: "33a", img: url+"48A" },
-        { id: 60, pieza: "34a", img: url+"48A" },
-        { id: 61, pieza: "35a", img: url+"48A" },
-        { id: 62, pieza: "36a", img: url+"48A" },
-        { id: 63, pieza: "37a", img: url+"48A" },
-        { id: 64, pieza: "38a", img: url+"48A" },
+        { id: 49, pieza: "48a", img: url + "48A.png" },
+        { id: 50, pieza: "47a", img: url + "47A.png" },
+        { id: 51, pieza: "46a", img: url + "46A.png" },
+        { id: 52, pieza: "45a", img: url + "45A.png" },
+        { id: 53, pieza: "44a", img: url + "44A.png" },
+        { id: 54, pieza: "43a", img: url + "43A.png" },
+        { id: 55, pieza: "42a", img: url + "42A.png" },
+        { id: 56, pieza: "41a", img: url + "41A.png" },
+        { id: 57, pieza: "31a", img: url + "31A.png" },
+        { id: 58, pieza: "32a", img: url + "32A.png" },
+        { id: 59, pieza: "33a", img: url + "33A.png" },
+        { id: 60, pieza: "34a", img: url + "34A.png" },
+        { id: 61, pieza: "35a", img: url + "35A.png" },
+        { id: 62, pieza: "36a", img: url + "36A.png" },
+        { id: 63, pieza: "37a", img: url + "37A.png" },
+        { id: 64, pieza: "38a", img: url + "38A.png" },
       ],
     },
     {
       tooth: [
-        { id: 65, pieza: "48b", img: "" },
-        { id: 66, pieza: "47b", img: "" },
-        { id: 67, pieza: "46b", img: "" },
-        { id: 68, pieza: "45b", img: "" },
-        { id: 69, pieza: "44b", img: "" },
+        { id: 65, pieza: "48b", img: url + "48B.png" },
+        { id: 66, pieza: "47b", img: url + "47B.png" },
+        { id: 67, pieza: "46b", img: url + "46B.png" },
+        { id: 68, pieza: "45b", img: url + "45B.png" },
+        { id: 69, pieza: "44b", img: url + "44B.png" },
         { id: 70, pieza: "", img: "" },
         { id: 71, pieza: "", img: "" },
         { id: 72, pieza: "", img: "" },
         { id: 73, pieza: "", img: "" },
         { id: 74, pieza: "", img: "" },
         { id: 75, pieza: "", img: "" },
-        { id: 76, pieza: "34b", img: "" },
-        { id: 77, pieza: "35b", img: "" },
-        { id: 78, pieza: "36b", img: "" },
-        { id: 79, pieza: "37b", img: "" },
-        { id: 80, pieza: "38b", img: "" },
+        { id: 76, pieza: "34b", img: url + "34B.png" },
+        { id: 77, pieza: "35b", img: url + "35B.png" },
+        { id: 78, pieza: "36b", img: url + "36B.png" },
+        { id: 79, pieza: "37b", img: url + "37B.png" },
+        { id: 80, pieza: "38b", img: url + "38B.png" },
       ],
     },
     {
       tooth: [
-        { id: 81, pieza: "48c", img: "" },
-        { id: 82, pieza: "47c", img: "" },
-        { id: 83, pieza: "46c", img: "" },
-        { id: 84, pieza: "45c", img: "" },
-        { id: 85, pieza: "44c", img: "" },
-        { id: 86, pieza: "43c", img: "" },
-        { id: 87, pieza: "42c", img: "" },
-        { id: 88, pieza: "41c", img: "" },
-        { id: 89, pieza: "31c", img: "" },
-        { id: 90, pieza: "32c", img: "" },
-        { id: 91, pieza: "33c", img: "" },
-        { id: 92, pieza: "34c", img: "" },
-        { id: 93, pieza: "35c", img: "" },
-        { id: 94, pieza: "36c", img: "" },
-        { id: 95, pieza: "37c", img: "" },
-        { id: 96, pieza: "38c", img: "" },
+        { id: 81, pieza: "48c", img: url + "48C.png" },
+        { id: 82, pieza: "47c", img: url + "47C.png" },
+        { id: 83, pieza: "46c", img: url + "46C.png" },
+        { id: 84, pieza: "45c", img: url + "45C.png" },
+        { id: 85, pieza: "44c", img: url + "44C.png" },
+        { id: 86, pieza: "43c", img: url + "43C.png" },
+        { id: 87, pieza: "42c", img: url + "42C.png" },
+        { id: 88, pieza: "41c", img: url + "41C.png" },
+        { id: 89, pieza: "31c", img: url + "31C.png" },
+        { id: 90, pieza: "32c", img: url + "32C.png" },
+        { id: 91, pieza: "33c", img: url + "33C.png" },
+        { id: 92, pieza: "34c", img: url + "34C.png" },
+        { id: 93, pieza: "35c", img: url + "35C.png" },
+        { id: 94, pieza: "36c", img: url + "36C.png" },
+        { id: 95, pieza: "37c", img: url + "37C.png" },
+        { id: 96, pieza: "38c", img: url + "38C.png" },
       ],
     },
   ];
@@ -146,21 +146,44 @@ function TeethTable() {
     <>
       <Table>
         <Tbody>
+          {arrayTeethUp.map((t) => {
+            return (
+              <tr>
+                {t.tooth.map((a) => {
+                  return (
+                    <TdTable>
+                      <img src={a.img} />{" "}
+                    </TdTable>
+                  );
+                })}
+              </tr>
+            );
+          })}
           <tr>
-            <TdTable>Empaste</TdTable>
-
-            <TdTable> 18a</TdTable>
-
-            <TdTable>Sergio Hervás Aragón</TdTable>
-
-            <TdTable>50 €</TdTable>
-
-            <TdTable>
-              <BtnsTable className={"deleteTable"}>
-                <MdDelete />
-              </BtnsTable>
-            </TdTable>
+            {indiceUp.map((i) => {
+              return <TdTable>{i}</TdTable>;
+            })}
           </tr>
+
+          <tr>
+            {indiceDown.map((i) => {
+              return <TdTable>{i}</TdTable>;
+            })}
+          </tr>
+
+          {arrayTeethDown.map((t) => {
+            return (
+              <tr>
+                {t.tooth.map((a) => {
+                  return (
+                    <TdTable>
+                      <img src={a.img} />{" "}
+                    </TdTable>
+                  );
+                })}
+              </tr>
+            );
+          })}
         </Tbody>
       </Table>
     </>
--- api-vite-clinica/src/pages/patients/TherapyPatient.jsx
+++ api-vite-clinica/src/pages/patients/TherapyPatient.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import HeaderSection from "../../components/HeaderSection";
 import CardBasic from "../../components/CardBasic";
 import TableTherapyPatient from "../../components/patient/TableTherapyPatient";
+import TeethTable from "../../components/patient/TeethTable";
 
 function TherapyPatient() {
   return (
     <>
       <HeaderSection title={"Threapy Patient"} />
+      <div className="row">
+        <div className="col-sm-12 col-md-12 col-lg-12">
+          <CardBasic>
+            <TeethTable/>
+          </CardBasic>
+        </div>
+      </div>
       <div className="row">
         <div className="col-sm-12 col-md-12 col-lg-12">
           <CardBasic>
